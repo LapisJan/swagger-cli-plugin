f94199375ab30ee3eebe7a976296181b71c47a9d
LapisJan <ploypailin.wongpichit@kingpower.com>
2024-08-06 22:09:24 +0700

    2. Add @ApiProperty at req

--- src/users/dto/create-user-req.dto.ts
+++ src/users/dto/create-user-req.dto.ts
@@ -1,5 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger"
+
 export class CreateUserReqDto
 {
+    // @ApiProperty()
     firstname: string
+
+    // @ApiProperty({ required: true, type: 'string' })
     lastname: string
 }
--- src/users/dto/update-user-req.dto.ts
+++ src/users/dto/update-user-req.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger"
+
 export class UpdateUserReqDto {
+    @ApiProperty({ required: true, type: 'string' })
     firstname: string
+
+    @ApiProperty({ required: true, type: 'string' })
     lastname: string
 }
